42ed48da9cc667f6ca95ba42994611f6cb3304f8
Lori Rothermel <lori@rothermel1.com>
2024-12-08 18:51:27 -0800

    Initial Commit

---
+++ LoadImageFromInternet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoadImageFromInternet
+//
+//  Created by Lori Rothermel on 12/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadImageFromInternet/LoadImageFromInternetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImageFromInternetApp.swift
+//  LoadImageFromInternet
+//
+//  Created by Lori Rothermel on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadImageFromInternetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
